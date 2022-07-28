97a1ba5413a495544af2767d3554f8f6f136b622
jeongbin <qls2467@gmail.com>
2022-07-28 00:21:47 -0600

    HTTP server

--- crud_app/server.js
+++ crud_app/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const app = express();
 
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
+
+// log request
+app.use(morgan("tiny")); // * shoudl chek what is this
 
 app.get("/", (req, res) => {
   res.send("crud");
