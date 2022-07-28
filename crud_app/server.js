0536eb090da5667cac8636b25cdb9ecdd8bdb59b
jeongbin <qls2467@gmail.com>
2022-07-28 08:48:58 -0600

    Module

--- crud_app/server.js
+++ crud_app/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
 const app = express();
 
 dotenv.config({ path: "config.env" });
@@ -8,6 +10,19 @@ const PORT = process.env.PORT || 8080;
 
 // log request
 app.use(morgan("tiny")); // * shoudl chek what is this
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+//css/style.css
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 
 app.get("/", (req, res) => {
   res.send("crud");
