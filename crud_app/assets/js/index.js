d842a4c7a4e6b98e973bd10e144a686143c35783
jeongbin <qls2467@gmail.com>
2022-08-01 10:00:24 -0600

    Delete user

--- crud_app/assets/js/index.js
+++ crud_app/assets/js/index.js
@@ -22,3 +22,24 @@ $("#update_user").submit(function (event) {
     alert("Data Updated Successfully!");
   });
 });
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  //$ondelete  => create the val
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+    //attr == attribute
+
+    var request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
+    }
+  });
+}
