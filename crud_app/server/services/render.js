c7b5a06d955b542ee73440098ed7a4f3df48c538
jeongbin <qls2467@gmail.com>
2022-08-01 07:45:43 -0600

    Using API

--- crud_app/server/services/render.js
+++ crud_app/server/services/render.js
@@ -1,5 +1,19 @@
+const axios = require("axios");
+// this module is going to allow us to make a request
+
 exports.homeRoutes = (req, res) => {
-  res.render("index");
+  //Make a get request to /api/users
+  // this get request is going to return promise
+  axios
+    .get("http://localhost:3000/api/users")
+    .then((response) => {
+      console.log(response.data);
+      res.render("index", { users: response.data });
+      // first is index.js / second data has all the records of mongoDB
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 exports.add_user = (req, res) => {
